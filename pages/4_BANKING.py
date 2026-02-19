e053d7dd3c8179f6bb018b9dc01ce09ffa3feb10
Anashe-Masomeke <masomekeanashe4@gmail.com>
2026-02-19 09:14:53 +0200

    Adjust background size for watermark in banking page

--- pages/4_BANKING.py
+++ pages/4_BANKING.py
@@ -28,7 +28,7 @@ def add_watermark():
             background-image: url("data:image/png;base64,{logo_base64}");
             background-repeat: no-repeat;
             background-position: center;
-            background-size: 1500px;
+            background-size: 1000px;
             opacity: 0.1;   /* 🔥 control watermark visibility here */
             pointer-events: none;
             z-index: 0;
