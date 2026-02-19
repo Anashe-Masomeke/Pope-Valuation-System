01aa61c1d38e7ec32f22f6ad0ad0e6c768b0504b
Anashe-Masomeke <masomekeanashe4@gmail.com>
2026-02-19 10:26:29 +0200

    Update DASHBOARD.py

--- DASHBOARD.py
+++ DASHBOARD.py
@@ -10,19 +10,20 @@ def add_watermark():
 
         watermark_css = f"""
         <style>
+
         /* Make watermark very light */
         .stApp::before {{
             content: "";
             position: fixed;
-            top: 0;
-            left: 0;
+            top: 40;
+            left: 50;
             width: 100%;
             height: 100%;
             background-image: url("data:image/png;base64,{logo_base64}");
             background-repeat: no-repeat;
             background-position: center;
-            background-size: 1000px;
-            opacity: 0.1;   /* 🔥 control watermark visibility here */
+            background-size: 1500px;
+            opacity: 0.07;   /* 🔥 control watermark visibility here */
             pointer-events: none;
             z-index: 0;
         }}
@@ -178,3 +179,4 @@ st.markdown("""
     Powered by <b>FBC Securities</b> • Investment Research & Valuation Dashboard
 </div>
 """, unsafe_allow_html=True)
+
