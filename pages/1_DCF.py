68415bc6bce70c76405291e02b4a6c5ae0785d42
Anashe-Masomeke <masomekeanashe4@gmail.com>
2026-04-17 14:47:15 +0200

    Update 1_DCF.py

--- pages/1_DCF.py
+++ pages/1_DCF.py
@@ -1201,7 +1201,12 @@ st.markdown("""
     fill: white !important;
     color: white !important;
 }
-
+/* FORCE white icon */
+button[kind="secondary"] svg,
+button[kind="primary"] svg {
+    fill: white !important;
+    color: white !important;
+}
 /* Ensure text is also white (just in case) */
 .stDownloadButton button {
     color: white !important;
